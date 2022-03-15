ed16b736f50cd553680852ce63da7349fac3c338
RiyandiDjohari <riyandidjohari8@gmail.com>
2022-03-15 13:32:21 +0700

    Fix: Home.js Improve Display Home

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
 
 class Home extends React.Component {
   render() {
     return (
-      <div class="home-background">
+      <div className="home-background">
         <header>
           <div class="container">
             <div class="row align-items-center">
-              <div class="col-lg-8 col-md-12 col-sm-12 col-12">
+              <div class="col-lg-7 col-md-12 col-sm-12 col-12">
                 <div class="alterra">
                   <a href="index.html">
-                    <img class="logo-atas" src="asset/img/logo-ALTA@2x.png" />
+                    <img class="logo-atas" src="asset/img/logo-ALTA@2x.png" alt="logo-alta" />
                   </a>
                 </div>
               </div>
-              <div class="col-lg-4 col-md-12 col-sm-12 col-12">
+              <div class="col-lg-5 col-md-12 col-sm-12 col-12">
                 <div class="container">
                   <div class="row align-items-center navig">
-                    <div class="col-lg-3 col-md-3 col-sm-3 text-md-center text-sm-center text-lg-right">
+                    <div class=" nav-item col-lg-3 col-md-3 col-sm-3 text-md-center text-sm-center text-lg-right px-0">
                       <a href="index.html" class="aktif">
                         HOME
                       </a>
                     </div>
-                    <div class="col-lg-3 col-md-3 col-sm-3 text-md-center text-sm-center text-lg-center">
+                    <div class=" nav-item col-lg-3 col-md-3 col-sm-3 text-md-center text-sm-center text-lg-center px-0">
                       <a href="about.html">ABOUT</a>
                     </div>
-                    <div class="col-lg-3 col-md-3 col-sm-3 text-md-center text-sm-center text-lg-left">
-                      <a href="#">EXPERIENCE</a>
+                    <div class=" nav-item col-lg-3 col-md-3 col-sm-3 text-md-center text-sm-center text-lg-center px-0">
+                      <a href="/">EXPERIENCE</a>
                     </div>
-                    <div class="col-lg-3 col-md-3 col-sm-3 text-md-center text-sm-center text-lg-right">
+                    <div class=" nav-item col-lg-3 col-md-3 col-sm-3 text-md-center text-sm-center text-lg-right px-0">
                       <a href="form.html">CONTACT</a>
                     </div>
                   </div>
@@ -40,7 +40,7 @@ class Home extends React.Component {
         <div class="container h-100">
           <div class="row align-items-center isi-home">
             <div class="col-lg-4 col-md-12 col-sm-12  col-12">
-              <img id="foto-profil" src="asset/img/matthew-hamilton-tNCH0sKSZbA-unsplash.jpg" />
+              <img id="foto-profil" src="asset/img/matthew-hamilton-tNCH0sKSZbA-unsplash.jpg" alt="profil" />
             </div>
             <div class="col-lg-8 col-md-12 col-sm-12 col-12">
               <div class="main-p">
